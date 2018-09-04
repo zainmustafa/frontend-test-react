c18f11d259ed7afa9fc5d92b27fb0ac5a305e05f
zainmustafa <zmdhillo@gmail.com>
2018-09-04 16:31:14 +0500

    add todo reducer details.

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -1,1 +1,27 @@
-export default (state = {}, action) => state;
+import forEach from "lodash/forEach";
+import ActionType from "../actions/actionsTypes.js";
+
+const initialState = {
+    isLoading: false,
+    todoList : []
+};
+
+export default (state = initialState, action) => {
+    switch (action.type) {
+        case ActionType.GET_TODO:
+            return { ...state, isLoading: true };
+
+        case ActionType.GET_TODO_SUCCESS:
+            return {
+                ...state,
+                todoList : action.payload,
+                isLoading: false
+            };
+
+        case ActionType.GET_TODO_FAIL:
+            return { ...state, isLoading: false };
+
+        default:
+            return state;
+    }
+};
