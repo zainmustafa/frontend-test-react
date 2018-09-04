bca51d5a5a74f7eb571f2b0225a2dba7265733a7
zainmustafa <zmdhillo@gmail.com>
2018-09-04 15:14:09 +0500

    add listitem component

--- src/components/todolist/ListItem.jsx
+++ src/components/todolist/ListItem.jsx
@@ -1,3 +1,57 @@
 import React from "react";
+import PropTypes from "prop-types";
+import TodoDetails from "./TodoDetails.jsx";
 
-export default () => <div>List Item</div>;
+
+export default class ListItem extends React.Component {
+    state = {
+        showDetails: false
+    };
+
+    toggleDetails = () => {
+        this.setState({
+            showDetails: !this.state.showDetails
+        });
+    };
+
+    render() {
+        const { item } = this.props;
+        const tempDate = new Date(item.createdAt);
+
+        const date =
+            `${tempDate.getDate()
+            }/${
+                tempDate.getMonth()
+            }/${
+                tempDate.getFullYear()}`;
+
+        return (
+            <React.Fragment>
+                <div className="listitem" onClick={this.toggleDetails}>
+                    <div className="listitem-left">
+                        <span className="listitem-left-date">{date}</span>
+                        <span className="listitem-left-title">
+                            {item.title}
+                        </span>
+                    </div>
+                    <div className="listitem-right">
+                        <input
+                            className="listitem-right-check"
+                            type="checkbox"
+                            name="vehicle"
+                            value="Car"
+                            checked={item.isDone}
+                        />
+                    </div>
+                </div>
+                {this.state.showDetails ? (
+                    <TodoDetails display={this.state.showDetails} id={item.id} />
+                ) : null}
+            </React.Fragment>
+        );
+    }
+}
+
+ListItem.propTypes = {
+    item: PropTypes.object.isRequired
+}
