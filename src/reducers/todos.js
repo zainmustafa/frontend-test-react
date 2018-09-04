704e787c3c67f02c3a72503fd197cb9b0b8d889a
zainmustafa <zmdhillo@gmail.com>
2018-09-04 17:48:39 +0500

    add todo detail reducer.

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -3,22 +3,55 @@ import ActionType from "../actions/actionsTypes.js";
 
 const initialState = {
     isLoading: false,
-    nodeList : []
+    nodeList: [],
+    nodeDetail: {
+        id : "",
+        title : "",
+        description :"",
+        priority : 1,
+        tag : [],
+        createdAt : "",
+        isDone : false
+    }
 };
 
 export default (state = initialState, action) => {
     switch (action.type) {
+        // Get Notes
         case ActionType.GET_TODO:
             return { ...state, isLoading: true };
 
         case ActionType.GET_TODO_SUCCESS:
             return {
                 ...state,
-                nodeList : action.payload,
+                nodeList: action.payload,
                 isLoading: false
             };
 
         case ActionType.GET_TODO_FAIL:
+            return { ...state, isLoading: false };
+        
+        // Get Note Details
+        case ActionType.GET_TODO_DETAIL:
+            const tempNodeDetail = {
+                id : "",
+                title : "",
+                description :"",
+                priority : 1,
+                tag : [],
+                createdAt : "",
+                isDone : false
+            }
+            return { ...state, nodeDetail : tempNodeDetail, isLoading: true };
+
+        case ActionType.GET_TODO_DETAIL_SUCCESS:
+            return {
+                ...state,
+                nodeDetail: action.payload,
+                isLoading: false
+            };
+
+        case ActionType.GET_TODO_DETAIL_FAIL:
             return { ...state, isLoading: false };
 
         default:
