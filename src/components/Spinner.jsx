2be58bbfeac58b035b3ffa40fd94521f714667b3
zainmustafa <zmdhillo@gmail.com>
2018-09-04 17:49:49 +0500

    add spinner component

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default () => {
+    return (
+        <div id="spinner">
+            <img src="/public/spinner.gif" alt="Spinner" align="middle" />
+        </div>
+    );
+};
